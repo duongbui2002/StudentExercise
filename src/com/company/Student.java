5fd713ddc2472cb7fdecc140e57b42790a5e68d3
Bui Dinh Duong <duongbuidinh593@gmail.com>
2021-09-21 16:35:52 +0700

    push

---
+++ src/com/company/Student.java
@@ -0,0 +1,74 @@
+package com.company;
+
+public class Student {
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInfo() {
+        String info = this.name + " - " + this.id + " - " + this.group + " - " + this.email;
+        return info;
+    }
+
+    //y 4:
+    public Student() {
+        this.name = "Student";
+        this.id = "000";
+        this.group = "K62CB";
+        this.email = "uet@vnu.edu.vn";
+    }
+
+    public Student(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.group = "K62CB";
+    }
+
+    public Student(Student student) {
+        this.email = student.getEmail();
+        this.id = student.getId();
+        this.group = student.getGroup();
+        this.name = student.getName();
+    }
+
+    public Student(String name, String id, String group, String email) {
+        this.name = name;
+        this.id = id;
+        this.group = group;
+        this.email = email;
+    }
+}
